baa9e9e6abc2ac33502ea8770c21ffee4bf19b92
alxmtzr <ametzler188@googlemail.com>
2024-12-13 15:17:19 +0100

    added dropdown and date picker

--- app/src/main/java/de/alxmtzr/freshify/HomeFragment.java
+++ app/src/main/java/de/alxmtzr/freshify/HomeFragment.java
@@ -1,58 +1,78 @@
 package de.alxmtzr.freshify;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
+
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initCategoryDropDown(view);
+        initExpiryDateField(view);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    private void initCategoryDropDown(@NonNull View view) {
+        AutoCompleteTextView dropdownMenu = view.findViewById(R.id.categoryDropdownMenu);
+
+        // data source for categories
+        String[] items = new String[]{"Option 1", "Option 2", "Option 3"};
+
+        // create adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                requireContext(),
+                R.layout.category_dropdown_item,
+                R.id.categoryDropdownTextView,
+                items
+        );
+
+        dropdownMenu.setAdapter(adapter);
+    }
+
+    private void initExpiryDateField(View view) {
+        TextInputEditText expiryDateEditText = view.findViewById(R.id.expiryDateEditText);
+
+        // click listener to show calendar
+        expiryDateEditText.setOnClickListener(v -> {
+            // current date
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            // create date picker dialog
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    requireContext(),
+                    (view1, selectedYear, selectedMonth, selectedDay) -> {
+                        // format date
+                        String formattedDate = selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear;
+                        expiryDateEditText.setText(formattedDate);
+                    },
+                    year, month, day
+            );
+
+            // show date picker
+            datePickerDialog.show();
+        });
     }
 
     @Override
