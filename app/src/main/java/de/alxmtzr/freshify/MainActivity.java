9b9037d275c388cbd4c934709ac6752b99215405
alxmtzr <ametzler188@googlemail.com>
2024-12-13 11:05:09 +0100

    added fragments and toolbar

--- app/src/main/java/de/alxmtzr/freshify/MainActivity.java
+++ app/src/main/java/de/alxmtzr/freshify/MainActivity.java
@@ -2,11 +2,10 @@ package de.alxmtzr.freshify;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initBottomNavBar(savedInstanceState);
+    }
+
+    private void initBottomNavBar(Bundle savedInstanceState) {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        // set home fragment as default
+        if (savedInstanceState != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            // determine which fragment to show
+            if (item.getItemId() == R.id.home) {
+                selectedFragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.fridge) {
+                selectedFragment = new FridgeFragment();
+            } else if (item.getItemId() == R.id.settings) {
+                selectedFragment = new SettingsFragment();
+            }
+
+            // show the selected fragment
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+        });
     }
 }
