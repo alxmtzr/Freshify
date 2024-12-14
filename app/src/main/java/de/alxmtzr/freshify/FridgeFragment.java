1258a48a5adef8d2e8d7c797a0770ec852a4b4f5
alxmtzr <ametzler188@googlemail.com>
2024-12-14 14:13:24 +0100

    added chips with adapter

--- app/src/main/java/de/alxmtzr/freshify/FridgeFragment.java
+++ app/src/main/java/de/alxmtzr/freshify/FridgeFragment.java
@@ -5,12 +5,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.alxmtzr.freshify.adapter.ItemsAdapter;
+import de.alxmtzr.freshify.data.local.CategoryDatabase;
+import de.alxmtzr.freshify.data.local.FreshifyRepository;
+import de.alxmtzr.freshify.data.local.impl.CategoryDatabaseImpl;
+import de.alxmtzr.freshify.data.local.impl.FreshifyDBHelper;
+import de.alxmtzr.freshify.data.local.impl.FreshifyRepositoryImpl;
+import de.alxmtzr.freshify.data.model.ItemEntity;
 
 public class FridgeFragment extends Fragment {
+
+    private FreshifyRepository repository;
+    private ItemsAdapter itemsAdapter;
 
     public FridgeFragment() {
         // Required empty public constructor
@@ -25,8 +45,66 @@ public class FridgeFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        RecyclerView itemsRecyclerView;
         super.onViewCreated(view, savedInstanceState);
 
+        FreshifyDBHelper dbHelper = new FreshifyDBHelper(getContext());
+        repository = new FreshifyRepositoryImpl(dbHelper);
 
+        itemsRecyclerView = view.findViewById(R.id.itemsRecyclerView);
+        itemsRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        // Adapter
+        itemsAdapter = new ItemsAdapter(new ArrayList<>());
+        itemsRecyclerView.setAdapter(itemsAdapter);
+
+        initCategoryChips(view);
+
+    }
+
+    private void initCategoryChips(View view) {
+        ChipGroup catgegoryChipGroup = view.findViewById(R.id.categoryChipGroup);
+
+        // get categories from database
+        CategoryDatabase categoryDatabase = new CategoryDatabaseImpl(requireContext());
+        List<String> categories = categoryDatabase.getCategories();
+
+        for (String category : categories) {
+            Chip chip = new Chip(requireContext());
+            chip.setText(category);
+            chip.setCheckable(true);
+            chip.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                // filter items by category
+                if (isChecked) {
+                    filterItemsByCategory(categoryDatabase, category);
+                } else {
+                    resetFilter();
+                }
+            });
+
+            catgegoryChipGroup.addView(chip);
+        }
+    }
+
+    private void filterItemsByCategory(CategoryDatabase categoryDatabase, String categoryName) {
+        int categoryId = categoryDatabase.getIdByCategory(categoryName);
+
+        // get items by category from database
+        List<ItemEntity> filteredItems = repository.getItemsByCategories(List.of(categoryId));
+
+        // Adapter aktualisieren
+        itemsAdapter.updateItems(filteredItems);
+
+        Toast.makeText(requireContext(), "Filtered by: " + categoryName, Toast.LENGTH_SHORT).show();
+    }
+
+    private void resetFilter() {
+        // show all items
+        List<ItemEntity> allItems = repository.getAllItems();
+
+        // refresh adapter
+        itemsAdapter.updateItems(allItems);
+
+        Toast.makeText(requireContext(), "Filter reset", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/de/alxmtzr/freshify/adapter/ItemsAdapter.java
@@ -0,0 +1,62 @@
+package de.alxmtzr.freshify.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import de.alxmtzr.freshify.R;
+import de.alxmtzr.freshify.data.model.ItemEntity;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
+
+    private List<ItemEntity> items;
+
+    public ItemsAdapter(List<ItemEntity> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_row, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        ItemEntity item = items.get(position);
+        holder.itemName.setText(item.getName());
+        holder.itemQuantity.setText(String.valueOf(item.getQuantity()));
+        holder.itemCategory.setText(item.getCategoryName());
+        holder.itemExpiryDate.setText(item.getExpiryDate().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void updateItems(List<ItemEntity> newItems) {
+        this.items = newItems;
+        notifyDataSetChanged();
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+        TextView itemName, itemQuantity, itemCategory, itemExpiryDate;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemName = itemView.findViewById(R.id.itemName);
+            itemQuantity = itemView.findViewById(R.id.itemQuantity);
+            itemCategory = itemView.findViewById(R.id.itemCategory);
+            itemExpiryDate = itemView.findViewById(R.id.itemExpiryDate);
+        }
+    }
+}
+
