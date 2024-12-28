0f1eee26813e7d68dab9d65c93198e209ca6bd5b
alxmtzr <ametzler188@googlemail.com>
2024-12-28 12:41:13 +0100

    added NotificationService

---
+++ app/src/main/java/de/alxmtzr/freshify/notification/NotificationService.java
@@ -0,0 +1,106 @@
+package de.alxmtzr.freshify.notification;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import java.util.List;
+
+import de.alxmtzr.freshify.R;
+import de.alxmtzr.freshify.data.model.ItemEntity;
+
+public class NotificationService {
+
+    private static final int NOTIFY_SINGLE_EXPIRING_ITEM_ID = 1;
+    private static final int NOTIFY_MULTIPLE_EXPIRING_ITEMS_ID = 2;
+    private static final int NOTIFY_SINGLE_EXPIRED_ITEM_ID = 3;
+    private static final int NOTIFY_MULTIPLE_EXPIRED_ITEMS_ID = 4;
+
+    private static final String CHANNEL_ID = "freshify_notifications";
+    private static final String CHANNEL_DESCRIPTION = "Show notifications for expired or expiring items";
+    private final Context context;
+    private final NotificationManager notificationManager;
+
+    public NotificationService(Context context) {
+        this.context = context;
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_DESCRIPTION,
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void notifyExpiredItems(List<ItemEntity> expiredItems) {
+        if (expiredItems.isEmpty()) {
+            return; // no notification if no items have expired
+        }
+
+        if (expiredItems.size() == 1) {
+            // notify user about a single expired item
+            ItemEntity item = expiredItems.get(0);
+            sendNotification(
+                    context.getString(R.string.item_expired),
+                    context.getString(R.string.the_item)
+                            + item.getName()
+                            + context.getString(R.string.has_expired),
+                    NOTIFY_SINGLE_EXPIRING_ITEM_ID
+            );
+        } else {
+            // notify user about multiple expired items
+            sendNotification(
+                    context.getString(R.string.multiple_items_expired),
+                    expiredItems.size() + context.getString(R.string.items_have_expired_check_your_list),
+                    NOTIFY_MULTIPLE_EXPIRING_ITEMS_ID
+            );
+        }
+    }
+
+    public void notifyExpiringItems(List<ItemEntity> expiringItems) {
+        if (expiringItems.isEmpty()) {
+            return; // no notification if no items have expired
+        }
+
+        if (expiringItems.size() == 1) {
+            // notify user about a single expiring item
+            ItemEntity item = expiringItems.get(0);
+            sendNotification(
+                    context.getString(R.string.item_expiring_soon),
+                    context.getString(R.string.the_item)
+                            + item.getName()
+                            + context.getString(R.string.is_expiring_soon),
+                    NOTIFY_SINGLE_EXPIRED_ITEM_ID
+            );
+        } else {
+            // notify user about multiple expiring items
+            sendNotification(
+                    context.getString(R.string.multiple_items_expiring_soon),
+                    expiringItems.size() + context.getString(R.string.items_are_expiring_soon_check_your_list),
+                    NOTIFY_MULTIPLE_EXPIRED_ITEMS_ID
+            );
+        }
+    }
+
+    private void sendNotification(String title, String message, int id) {
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_popup_reminder)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .build();
+        notificationManager.notify(id, notification);
+    }
+}
+
+
